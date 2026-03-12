352fa900e4aff10f252a84822421ccc83bc054f8
jon <jon.halquist@gmail.com>
2026-03-12 16:30:13 -0400

    updated portfolio link for audioHaze app

--- frontend/src/components/InfoCell/InfoCellProjects.js
+++ frontend/src/components/InfoCell/InfoCellProjects.js
@@ -15,7 +15,7 @@ const InfoCellProjects = ({ props }) => {
 
       <div className='infoOuterContainerRight'>
             <div className='photoContainer'>
-              <a id='image2container' href='https://audiohaze.herokuapp.com/' target="_blank">
+              <a id='image2container' href='https://audiohaze.onrender.com/' target="_blank">
                 <img id='highlightImage2' src={audiohaze} />
               </a>
             </div>
@@ -23,7 +23,7 @@ const InfoCellProjects = ({ props }) => {
           <div className='cellTitleText'>
             {/* <LogoSmall /> */}
             <div className='fillLineHorizontal'></div>
-            <a href='https://audiohaze.herokuapp.com/' target="_blank">
+            <a href='https://audiohaze.onrender.com/' target="_blank">
               Audiohaze
             </a>
           </div>
@@ -75,66 +75,6 @@ const InfoCellProjects = ({ props }) => {
       </div>
 
       <div className='spacerDivSmall'></div>
-
-      <div className='infoOuterContainerRight'>
-            <div className='photoContainer'>
-              <a id='image2container' href='https://wineauxapp.herokuapp.com/login' target="_blank">
-                <img id='highlightImage2' src={wineaux} />
-              </a>
-            </div>
-        <div className='textContainer'>
-          <div className='cellTitleText'>
-            {/* <LogoSmall /> */}
-            <div className='fillLineHorizontal'></div>
-            <a href='https://wineauxapp.herokuapp.com/login' target="_blank">
-              Wineaux
-            </a>
-          </div>
-          <div className='cellContentContainer'>
-            <div className='cellContent'>
-              {/* {props.content.map((el) => <p className='cellContentText' key={el[1] + el[2]}>{el}</p>)} */}
-              <p className='cellContentText'>
-              Wineaux is an app that takes inspiration from Untappd.
-              Users can share their impressions of various wines and browse
-              the wine discoveries and reviews of other users.
-              </p>
-            </div>
-            <div className='fillLineVertical'></div>
-          </div>
-        </div>
-      </div>
-
-      <div className='spacerDivSmall'></div>
-
-      <div className='infoOuterContainer'>
-        <div className='photoContainer'>
-          <a id='image2container' href='https://oraql.herokuapp.com/' target="_blank">
-            <img id='highlightImage2' src={oraql}/>
-          </a>
-        </div>
-        <div className='textContainer'>
-          <div className='cellTitleText'>
-          <a href='https://oraql.herokuapp.com/' target="_blank">
-              Oraql
-            </a>
-            <div className='fillLineHorizontal'></div>
-          </div>
-          <div className='cellContentContainer'>
-            <div className='fillLineVertical'></div>
-            <div className='cellContent'>
-              <p className='cellContentText'>
-              Oraql is a website for users to ask and answer
-              questions about supernatural phenomena. It is
-              inspired by Quora.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className='spacerDivSmall'></div>
-
-
 
     </>
   )
